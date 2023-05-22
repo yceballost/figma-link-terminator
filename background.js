85bd53bd6be18060e978dd639d9e27069d57be7f
yceballost <yceballost@gmail.com>
2023-05-22 21:38:21 +0200

    added ui and new features

--- background.js
+++ background.js
@@ -1,7 +1,20 @@
+var tiempoCierre = 6; // Valor predeterminado
+
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+  if (message.action === "actualizarTiempoCierre") {
+    tiempoCierre = message.tiempoCierre;
+  }
+});
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.url && tab.url.includes("https://www.figma.com/file/")) {
     setTimeout(function () {
       chrome.tabs.remove(tabId);
-    }, 10000);
+    }, tiempoCierre * 1000);
   }
+});
+
+chrome.browserAction.onClicked.addListener(function (tab) {
+  chrome.browserAction.setPopup({ popup: "popover.html" });
+  chrome.browserAction.openPopup();
 });
